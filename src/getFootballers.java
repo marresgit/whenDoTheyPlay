5d9b1bcb5a9e0129843cdeb20a7730b2103113d6
marresgit <saeztoro.mauricio@gmail.com>
2018-08-04 01:34:59 +0200

    Fixed first task - Making the project take input and restrict the input

--- src/getFootballers.java
+++ src/getFootballers.java
@@ -10,26 +10,39 @@ public class getFootballers {
         System.out.println("Enter football player: ");
         String footballer = reader.nextLine(); // Scans the next token of the input.
         reader.close();
-        String strarray[] = footballer.split(" ");
+        String strFootballerArray[] = footballer.split(" ");
 
+        String footballerName = "";
 
+        if (strFootballerArray.length == 1) {
+            footballerName = strFootballerArray[0];
+        }else if (strFootballerArray.length == 2) {
+            footballerName = strFootballerArray[0] + "+" + strFootballerArray[1];
+        } else {
+            System.out.println("This mini project only takes the <First/Last> name or <First and Last> name of the footballer, not more.");
+            System.exit(0);
+        }
 
-        URL soccerway = new URL("https://us.soccerway.com/search/?q=zlatan+ibrahimovic&module=all");
+        URL soccerway = new URL("https://us.soccerway.com/search/?q=" + footballerName + "&module=all");
         BufferedReader in = new BufferedReader(new InputStreamReader(soccerway.openStream()));
 
-        String inputline;
-        while ((inputline = in.readLine()) != null)
-            System.out.println(inputline);
-        in.close();
+            String inputline;
+            while ((inputline = in.readLine()) != null)
+                System.out.println(inputline);
+            in.close();
 
-
-
-        System.out.println(Arrays.toString(strarray));
+        // Debug prints
+        System.out.println("Footballername " + footballerName);
+        System.out.println(Arrays.toString(strFootballerArray));
+        System.out.println("Input length: " + strFootballerArray.length);
+        System.out.println("Final soccer URL: " + soccerway);
     }
 
 }
 
-// Notes: Fixa så att scriptet kollar ifall input blir Namn och efternamn eller bara ett namn/efternamn.
+// Notes/Tasks:
+// DONE - Fixa så att scriptet kollar ifall input blir Namn och efternamn eller bara ett namn/efternamn.
+//
 
 
 
